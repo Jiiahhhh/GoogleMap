43babfa127a36823119fa760b1efa57a69902499
Jiiahhhh <ijlalnurhadi01@gmail.com>
2022-12-23 16:41:52 +0800

    Location Permission

--- app/src/main/java/com/ilal/googlemap/MainActivity.kt
+++ app/src/main/java/com/ilal/googlemap/MainActivity.kt
@@ -1,15 +1,27 @@
 package com.ilal.googlemap
 
+import android.Manifest
+import android.annotation.SuppressLint
 import android.content.pm.PackageManager
+import android.location.Location
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.PersistableBundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.MapView
 import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.gms.tasks.Task
 import com.ilal.googlemap.databinding.ActivityMainBinding
-import java.util.jar.Manifest
 
 class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
@@ -17,6 +29,11 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
     lateinit var mapView: MapView
     private val MAP_VIEW_BUNDLE_KEY = "MapViewBundleKey"
     private lateinit var binding: ActivityMainBinding
+    private val DEFAULT_ZOOM = 15f
+    private var fusedLocationProviderClient: FusedLocationProviderClient? = null
+
+    val FINE_LOCATION_RQ = 101
+    val CAMERA_RQ = 102
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,6 +41,8 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         setContentView(binding.root)
 
         mapView = binding.maps
+
+        checkForPermissions(Manifest.permission.ACCESS_FINE_LOCATION, "location", FINE_LOCATION_RQ)
 
         var mapViewBundle: Bundle? = null
         if (savedInstanceState != null) {
@@ -54,12 +73,94 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
 
-        //askPermissionLocation()
+        checkForPermissions(Manifest.permission.ACCESS_FINE_LOCATION, "location", FINE_LOCATION_RQ)
         var mapViewBundle = outState.getBundle(MAP_VIEW_BUNDLE_KEY)
         if (mapViewBundle == null) {
             mapViewBundle = Bundle()
             outState.putBundle(MAP_VIEW_BUNDLE_KEY, mapViewBundle)
         }
         mapView.onSaveInstanceState(mapViewBundle)
+    }
+
+    private fun getCurrentLocation() {
+        fusedLocationProviderClient =
+            LocationServices.getFusedLocationProviderClient(this@MainActivity)
+
+        try {
+            @SuppressLint("MissingPermission") val location =
+                fusedLocationProviderClient!!.getLastLocation()
+            location.addOnCompleteListener(object : OnCompleteListener<Location> {
+                override fun onComplete(loc: Task<Location>) {
+                    if (loc.isSuccessful) {
+                        val currentLocation = loc.result as Location?
+                        if (currentLocation != null) {
+                            moveCamera(
+                                LatLng(currentLocation.latitude, currentLocation.longitude),
+                                DEFAULT_ZOOM
+                            )
+                        }
+                    } else {
+                        Toast.makeText(
+                            this@MainActivity,
+                            "Lokasi saat ini tidak diketahui",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }
+            })
+        } catch (se: java.lang.Exception) {
+            Log.e("TAG", "Security Exception")
+        }
+    }
+
+    private fun moveCamera(latLng: LatLng, zoom: Float) {
+        mMap!!.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, zoom))
+    }
+
+    private fun checkForPermissions(permission: String, name: String, requestCode: Int){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            when{
+                ContextCompat.checkSelfPermission(applicationContext, permission) == PackageManager.PERMISSION_GRANTED -> {
+                    getCurrentLocation()
+                    Toast.makeText(applicationContext, "$name permission granted", Toast.LENGTH_SHORT).show()
+                }
+                shouldShowRequestPermissionRationale(permission) -> showDialog(permission, name, requestCode)
+
+                else -> ActivityCompat.requestPermissions(this, arrayOf(permission), requestCode)
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        fun innerCheck(name:String){
+            if (grantResults.isEmpty() || grantResults[0] != PackageManager.PERMISSION_GRANTED){
+                Toast.makeText(applicationContext, "$name permission refuesd", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(applicationContext, "$name permission granted", Toast.LENGTH_SHORT).show()
+            }
+        }
+        when (requestCode) {
+            FINE_LOCATION_RQ -> innerCheck("location")
+            CAMERA_RQ -> innerCheck("camera")
+        }
+    }
+
+    private fun showDialog(permission: String, name: String, requestCode: Int){
+        val builder = AlertDialog.Builder(this)
+
+        builder.apply {
+            setMessage("Permission to access your $name is required to use this app")
+            setTitle("Permission Required")
+            setPositiveButton("OK") {dialog, which ->
+                ActivityCompat.requestPermissions(this@MainActivity, arrayOf(permission), requestCode)
+            }
+        }
+        val dialog = builder.create()
+        dialog.show()
     }
 }
